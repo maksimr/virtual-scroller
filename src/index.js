32a218f6b7df4e2489f82bfb933b9aaf6d406272
Maksim Ryzhikov <rv.maksim@gmail.com>
2019-11-28 16:49:47 +0300

    chore: clarify name

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ function main() {
   VirtualScroller.builder(document.getElementById('app'), {
     itemCount: 10000,
     itemBuilder() {
-      const placeholder = document.createElement('div');
-      placeholder.style.padding = '8px';
-      placeholder.innerHTML = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book`;
-      return placeholder;
+      const item = document.createElement('div');
+      item.style.padding = '8px';
+      item.innerHTML = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book`;
+      return item;
     }
   });
 }
