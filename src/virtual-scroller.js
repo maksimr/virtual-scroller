d15e80417d7a2c1efa8b51439d295a88438399fd
Maksim Ryzhikov <rv.maksim@gmail.com>
2020-02-17 21:21:58 +0300

    refactor: use request animation frame

--- src/virtual-scroller.js
+++ src/virtual-scroller.js
@@ -1,13 +1,11 @@
+import { SizeManager } from './size-manager';
+
+const BUFFER = 0.2;
+
 export class VirtualScroller {
   /**
-   * @typedef {{itemBuilder: Function<HTMLElement>, itemCount: number}} VirtualScrollerConfig
-   */
-  /**
-   * @typedef {{node: HTMLElement, height: <number|null>, observer: MutationObserver=}} VirtualItem
-   */
-  /**
    * @param {HTMLElement} node
-   * @param {VirtualScrollerConfig} params
+   * @param {{itemBuilder: Function<HTMLElement>, itemCount: number}} params
    * @return {VirtualScroller}
    */
   static builder(node, params) {
@@ -16,188 +14,153 @@ export class VirtualScroller {
 
   /**
    * @param {HTMLElement} rootNode
-   * @param {VirtualScrollerConfig} params
+   * @param {{itemBuilder: Function<HTMLElement>, itemCount: number}} params
    * @return {VirtualScroller}
    */
   constructor(rootNode, params) {
     this.rootNode = rootNode;
-    this.params = params;
-    /** @type {Object.<string, VirtualItem>} */
-    this.renderedItems = {};
+    this.itemBuilder = params.itemBuilder;
+    this.itemCount = params.itemCount;
+    this.sizeManager = new SizeManager();
+    this.rendered = {};
     this.start = 0;
     this.end = 0;
-    this.scroller = this._findScrollableNode(this.rootNode);
-
-    if (!this.params.hasOwnProperty('itemCount')) {
-      throw Error('itemCount is required');
-    }
-
-    if (!this.params.hasOwnProperty('itemBuilder')) {
-      throw Error('itemBuilder is required');
-    }
-
-    this.runway = document.createElement('div');
-    this.runway.style.width = this.runway.style.height = '1px';
-    this.runway.style.position = 'absolute';
-    this.runway.style.transform = 'translate(0, 0)';
-
-    this.rootNode.style.position = 'relative';
-    this.rootNode.appendChild(this.runway);
+    this.scroller = findScrollableAncestor(this.rootNode);
+    this.runway = renderRunway(this.rootNode);
 
     (this.scroller === document.documentElement ? window : this.scroller)
-      .addEventListener('scroll', () => this.onScroll());
-    window.addEventListener('resize', () => this.onResize());
+      .addEventListener('scroll', () => this.scheduleSync());
+    window.addEventListener('resize', () => {
+      this.sizeManager.reset();
+      this.scheduleSync();
+    });
 
-    this.onResize();
+    this.scheduleSync();
   }
 
-  onResize() {
-    const placeholder = this.params.itemBuilder(this.start);
-    placeholder.style.visibility = 'hidden';
-    this.rootNode.appendChild(placeholder);
-    this.placeholderSize = placeholder.offsetHeight;
-    this.rootNode.removeChild(placeholder);
-    this.runway.style.transform = `translateY(${this.placeholderSize * this.params.itemCount}px)`;
-
-    for (let i in this.renderedItems) {
-      if (this.renderedItems.hasOwnProperty(i)) {
-        this.renderedItems[i].height = null;
-      }
+  scheduleSync() {
+    if (this.syncRefId) {
+      return;
     }
 
-    this.performLayout();
+    this.syncRefId = window.requestAnimationFrame(() => {
+      this.syncRefId = null;
+      this.sync();
+    });
   }
 
-  onScroll() {
-    this.performLayout();
-  }
-
-  performLayout() {
-    const scrollTop = Math.max((this.scroller.scrollTop - this.rootNode.offsetTop), 0);
-    const start = Math.floor(scrollTop / this.placeholderSize);
+  sync() {
     const height = this.scroller.clientHeight;
-    const visible = Math.floor(height / this.placeholderSize);
-    this.fill(start, start + visible);
-  }
+    const buffer = height * BUFFER;
+    const scrollTop = Math.max(this.scroller.scrollTop - this.rootNode.offsetTop, 0);
+    const lowOffset = Math.max((scrollTop - buffer), 0);
+    const highOffset = scrollTop + height + buffer;
+    const averageSize = this.sizeManager.getAverageSize();
+    const startIndex = Math.max(Math.floor(lowOffset / averageSize), 0);
+    const endIndex = Math.min(Math.ceil(highOffset / averageSize), this.itemCount) - 1;
 
-  /**
-   * @param {number} start
-   * @param {number} end
-   */
-  fill(start, end) {
-    start = Math.max(start, 0);
-    end = Math.min(end, this.params.itemCount);
-
-    const fragment = document.createDocumentFragment();
-    for (let i = start; i <= end; i++) {
-      if (!this.renderedItems[i]) {
-        const itemNode = this.buildItem(i);
-        if (!itemNode) {
-          break;
-        }
+    let fragment = document.createDocumentFragment();
+    for (let i = startIndex; i <= endIndex; i++) {
+      if (!this.rendered[i]) {
+        const itemNode = renderItem(i, this.itemBuilder);
+        this.rendered[i] = itemNode;
         fragment.appendChild(itemNode);
+      } else if (fragment.children.length) {
+        this.rootNode.insertBefore(fragment, this.rendered[i]);
+        fragment = document.createDocumentFragment();
       }
     }
     this.rootNode.appendChild(fragment);
 
-    while (this.start < start) {
-      this.removeItem(this.start++);
+    while (this.start < startIndex) {
+      this.remove(this.start++);
     }
 
-    while (this.end > end) {
-      this.removeItem(this.end--);
+    while (this.end > endIndex) {
+      this.remove(this.end--);
     }
 
-    for (let i in this.renderedItems) {
-      if (this.renderedItems.hasOwnProperty(i)) {
-        const item = this.renderedItems[i];
-        if (item.height === null) {
-          item.height = item.node.offsetHeight;
+    for (let i in this.rendered) {
+      if (this.rendered.hasOwnProperty(i)) {
+        const node = this.rendered[i];
+        if (!this.sizeManager.has(node)) {
+          this.sizeManager.measure(node);
         }
       }
     }
 
-    let offset = start * this.placeholderSize;
-    for (let i in this.renderedItems) {
-      if (this.renderedItems.hasOwnProperty(i)) {
-        const item = this.renderedItems[i];
-        item.node.style.transform = `translateY(${offset}px)`;
-        offset += item.height;
+    let offset = startIndex * this.sizeManager.getAverageSize();
+    for (let i in this.rendered) {
+      if (this.rendered.hasOwnProperty(i)) {
+        const node = this.rendered[i];
+        node.style.transform = `translateY(${offset}px)`;
+        offset += this.sizeManager.getHopefulSize(node);
       }
     }
 
-    if (
-      (end < this.params.itemCount) &&
-      (start * this.placeholderSize + this.scroller.clientHeight) > offset) {
-      const newAverageSize = (offset - start * this.placeholderSize) / ((end + 1) - start);
-      const adjustment = Math.ceil(((start * this.placeholderSize + this.scroller.clientHeight) - offset) / newAverageSize);
-      this.fill(start, end + adjustment);
+    if ((endIndex < this.itemCount) && highOffset > offset) {
+      this.scheduleSync();
       return;
     }
 
-    this.start = start;
-    this.end = end;
-  }
-
-  /**
-   * @param {number} index
-   * @return {HTMLElement}
-   */
-  buildItem(index) {
-    const node = this.params.itemBuilder(index);
-
-    if (!node) {
-      return null;
+    this.start = startIndex;
+    this.end = endIndex;
+    if (averageSize !== this.sizeManager.getAverageSize()) {
+      this.runway.style.transform = `translateY(${this.sizeManager.getAverageSize() * this.itemCount}px)`;
     }
-
-    node.style.position = 'absolute';
-    node.style.left = node.style.right = '0px';
-    this.renderedItems[index] = {
-      node: node,
-      height: null
-    };
-
-    if (window.MutationObserver) {
-      const observer = new MutationObserver(() => {
-        this.onResize();
-      });
-      observer.observe(node, {characterData: true, childList: true, subtree: true});
-      this.renderedItems[index].observer = observer;
-    }
-
-    return node;
   }
 
   /**
    * @param {number} index
    */
-  removeItem(index) {
-    if (this.renderedItems[index]) {
-      const item = this.renderedItems[index];
-      if (item.observer) {
-        item.observer.disconnect();
-      }
-      this.rootNode.removeChild(item.node);
-      delete this.renderedItems[index];
+  remove(index) {
+    if (this.rendered[index]) {
+      const node = this.rendered[index];
+      this.sizeManager.remove(node);
+      this.rootNode.removeChild(node);
+      delete this.rendered[index];
     }
-  }
-
-  /**
-   * @param {HTMLElement} node
-   * @return {HTMLElement}
-   */
-  _findScrollableNode(node) {
-    while (
-      node.parentNode &&
-      node.parentNode !== document) {
-      const overflow = window.getComputedStyle(node).overflow;
-      if (overflow === 'visible' || overflow === '') {
-        node = node.parentNode;
-        continue;
-      }
-      break;
-    }
-    return node;
   }
 }
 
+/**
+ * @param {number} index
+ * @param {Function<HTMLElement>} itemBuilder
+ * @return {HTMLElement}
+ */
+function renderItem(index, itemBuilder) {
+  const node = itemBuilder(index);
+  node.style.position = 'absolute';
+  node.style.left = node.style.right = '0px';
+  return node;
+}
+
+/**
+ * @param {HTMLElement} rootNode
+ * @return {HTMLElement} Runway node
+ */
+function renderRunway(rootNode) {
+  const runwayNode = document.createElement('div');
+  runwayNode.style.width = runwayNode.style.height = '1px';
+  runwayNode.style.position = 'absolute';
+  runwayNode.style.transform = 'translate(0, 0)';
+  rootNode.style.position = 'relative';
+  rootNode.appendChild(runwayNode);
+  return runwayNode;
+}
+
+/**
+ * @param {HTMLElement} node
+ * @return {HTMLElement}
+ */
+function findScrollableAncestor(node) {
+  while (node.parentNode && node.parentNode !== document) {
+    const overflow = window.getComputedStyle(node).overflow;
+    if (overflow === 'visible' || overflow === '') {
+      node = node.parentNode;
+      continue;
+    }
+    break;
+  }
+  return node;
+}
--- src/virtual-scroller.test.js
+++ src/virtual-scroller.test.js
@@ -5,14 +5,16 @@ describe('VirtualScroller', function() {
   let params;
   beforeEach(function() {
     const containerHeight = 100;
+    document.documentElement.scrollTop = 0;
     node = document.createElement('div');
     node.style.height = `${containerHeight}px`;
     node.style.overflow = 'auto';
     document.body.appendChild(node);
     params = {
-      itemCount: 10,
+      itemCount: 100,
       itemBuilder: itemBuilder
     };
+    spyOn(window, 'requestAnimationFrame').and.callFake((cb) => cb());
   });
 
   it('should define service', function() {
@@ -28,31 +30,23 @@ describe('VirtualScroller', function() {
     VirtualScroller.builder(node, params);
     const items = node.querySelectorAll('.item');
 
-    expect(items.length).toEqual(11);
+    expect(items.length).toEqual(12);
     expect(items[0].style.transform).toEqual('translateY(0px)');
     expect(items[1].style.transform).toEqual(`translateY(${items[0].style.height})`);
   });
 
-  it('should correctly render visible items if items less then defined in itemsCount', function() {
-    VirtualScroller.builder(node, {
-      itemCount: params.itemCount,
-      itemBuilder(index) {
-        return index > 0 ? null : itemBuilder(index);
-      }
-    });
-    const items = node.querySelectorAll('.item');
-
-    expect(items.length).toEqual(1);
-    expect(items[0].style.transform).toEqual('translateY(0px)');
-  });
-
   it('should remove an item if it out of viewport after scroll', function() {
     VirtualScroller.builder(node, params);
-    scroll(node, 10);
+    scroll(node, 30);
 
-    const items = node.querySelectorAll('.item');
-    expect(items.length).toEqual(10);
+    let items = node.querySelectorAll('.item');
+    expect(items.length).toEqual(14);
     expect(items[0].innerText).toEqual('1');
+
+    scroll(node, 0);
+    items = node.querySelectorAll('.item');
+    expect(items.length).toEqual(12);
+    expect(items[0].innerText).toEqual('0');
   });
 
   it('should relayout items on resize', function() {
@@ -78,17 +72,17 @@ describe('VirtualScroller', function() {
       VirtualScroller.builder(node, params);
       const items = node.querySelectorAll('.item');
 
-      expect(items.length).toEqual(11);
+      expect(items.length).toEqual(12);
       expect(items[0].style.transform).toEqual('translateY(0px)');
       expect(items[1].style.transform).toEqual(`translateY(${items[0].style.height})`);
     });
 
     it('should render new items on viewport scroll', function() {
       VirtualScroller.builder(node, params);
-      scroll(window, 10);
+      scroll(window, 30);
 
       const items = node.querySelectorAll('.item');
-      expect(items.length).toEqual(10);
+      expect(items.length).toEqual(14);
       expect(items[0].innerText).toEqual('1');
     });
 
@@ -98,7 +92,7 @@ describe('VirtualScroller', function() {
       VirtualScroller.builder(node, params);
 
       const items = node.querySelectorAll('.item');
-      expect(items.length).toEqual(11);
+      expect(items.length).toEqual(12);
       expect(items[0].innerText).toEqual('0');
     });
   });
@@ -116,7 +110,7 @@ describe('VirtualScroller', function() {
       });
 
       const items = node.querySelectorAll('.item');
-      expect(items.length).toEqual(19);
+      expect(items.length).toEqual(23);
     });
   });
 
