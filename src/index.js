67a12832a111824d8b76e11adbebaf54010a4930
Maksim Ryzhikov <rv.maksim@gmail.com>
2020-03-08 01:25:56 +0300

    chore: add e2e tests

--- src/index.js
+++ src/index.js
@@ -6,12 +6,11 @@ function main() {
     bufferSize: 0,
     itemCount: count,
     itemBuilder(it) {
-      const icon = it % 2 ? '🦊' : '🐶';
       const item = document.createElement('div');
       item.style.padding = '8px';
       item.innerHTML = it % 2 ?
-        `<b>${it + 1}</b>/${count} - ${icon} Lorem Ipsum is simply dummy text` :
-        `<b>${it + 1}</b>/${count} - ${icon} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book`;
+        `<b>${it + 1}</b>/${count} - Lorem Ipsum is simply dummy text` :
+        `<b>${it + 1}</b>/${count} - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book`;
       return item;
     }
   });
---
+++ test/e2e/index.test.js
@@ -0,0 +1,57 @@
+const {toMatchImageSnapshot} = require('jest-image-snapshot');
+const config = require('../../package.json').config;
+expect.extend({toMatchImageSnapshot});
+
+describe('VirtualScroller', () => {
+  beforeEach(async () => {
+    await page.setViewport({width: 800, height: 600});
+  });
+
+  it('should render items', async () => {
+    await openTestPage();
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('should render only one item', async () => {
+    await openTestPage(1);
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('should scroll viewport', async () => {
+    await openTestPage();
+    await scrollTo(100);
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('should scroll to the bottom', async () => {
+    await openTestPage();
+    await scrollToBottom();
+    // Wait util we render item and do scroll correction
+    // because actual items size is bigger than expected
+    // so we should jump on the top to add extra scroll space
+    // for possibility to scroll at the last item
+    await page.waitForSelector('[index="2999998"]');
+    await scrollToBottom();
+
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot();
+  });
+
+  async function openTestPage(itemCount = 3000000) {
+    await page.goto(`${config.url}?${itemCount}`);
+  }
+
+  async function scrollToBottom() {
+    await scrollTo(Infinity);
+  }
+
+  async function scrollTo(scrollTop) {
+    await page.evaluate((scrollTop) => {
+      const appElement = document.getElementById('app');
+      appElement.scrollTop = isFinite(scrollTop) ? scrollTop : appElement.scrollHeight;
+    }, scrollTop);
+  }
+});
